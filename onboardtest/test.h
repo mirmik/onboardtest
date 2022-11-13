bb81d12c829f537ffe6f4fa762815c836b491784
mirmik <mirmikns@yandex.ru>
2022-11-13 21:50:37 +0300

    'add static word'

--- onboardtest/test.h
+++ onboardtest/test.h
@@ -5,9 +5,9 @@
 #include <onboardtest/testenv.h>
 
 #define TEST_CASE(name)                                                        \
-    void NOS_CONCAT2(test, __LINE__)();                                        \
+    static void NOS_CONCAT2(test, __LINE__)();                                        \
     static onboardtest::OnBoardTest NOS_CONCAT2(test_test_, __LINE__)(         \
         NOS_CONCAT2(test, __LINE__), name);                                    \
-    void NOS_CONCAT2(test, __LINE__)()
+    static void NOS_CONCAT2(test, __LINE__)()
 
 #endif
